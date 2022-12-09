48beda547eb047146831f0a2bdceea3a40eb6f40
rezafikkri <fikkri.reza@gmail.com>
2022-12-09 09:46:37 +0700

    feat: HOC localstorage

--- src/App.js
+++ src/App.js
@@ -1,32 +1,59 @@
 import React from 'react';
 import './App.css';
 
-// HOC: Higher-Order Components
+// HOC: Higher-Order Components - Local Storage
 
-const withUser = (WrappedComponent) => {
-  class WithUser extends React.Component {
-    constructor(props) {
-      super(props);
+function withLocalStorage(WrappedComponent) {
+  return class extends React.Component {
+    get(key) {
+      return localStorage.getItem(key);
+    }
 
-      this.state = {
-        user: 'RezaFikkri',
-      };
+    save(key, value) {
+      localStorage.setItem(key, value);
+    }
+
+    remove(key) {
+      localStorage.removeItem(key);
     }
 
     render() {
-      return <WrappedComponent user={this.state.user} {...this.props} />
+      return <WrappedComponent get={this.get} save={this.save} remove={this.remove} {...this.props} />
+    }
+  }
+}
+
+class User extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: ''
+    };
+  }
+
+  componentDidMount() {
+    const name = this.props.get('name');
+
+    if (!name) {
+      // load nama dari API/Database
+      this.props.save('name', 'user_random');
+      this.setState({
+        name: 'user_random'
+      });
+    } else {
+      this.setState({
+        name: this.props.get('name')
+      });
     }
   }
 
-  return WithUser;
-};
-
-function App(props) {
-  return (
-    <div>
-      I am app {props.user}
-    </div>
-  );
+  render() {
+    return (
+      <div>
+        I am app {this.state.name}
+      </div>
+    );
+  }
 }
 
-export default withUser(App);
+export default withLocalStorage(User);
